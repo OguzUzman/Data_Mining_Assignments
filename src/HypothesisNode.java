91747c07cf037ab949607e3d8cd7cb0bd367aba1
oguzmann <oguz.uzman93@gmail.com>
2016-05-24 18:56:31 +0200

    Added stuff

---
+++ src/HypothesisNode.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+/**
+ * Created by oguz on 24/05/16.
+ */
+public class HypothesisNode {
+
+    /**
+     * For every elemtnt of array suppose the length is 4, for every bit we have 3 conditions;
+     * -1 Don't care, (1)
+     * 0 Not
+     * 1 Normal
+     */
+    byte[] hypothesis;
+
+    List<HypothesisNode> children;
+
+
+
+
+}
